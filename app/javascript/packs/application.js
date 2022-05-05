18bb8210f1eef5c11d9f56afd93aa97c8131dc04
Trevor Sinnott <sinnott.trevor@gmail.com>
2022-05-05 14:26:19 +0900

    install bootstrap with webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,12 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import 'jquery'
+import 'popper.js'
+import 'bootstrap'
+
+import 'css/site'
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
